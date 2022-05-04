0d9f5eceab93747be9b0e4d0a013a4bd33e32288
Ильшат Галиев <galiew.work@mail.ru>
2022-05-04 18:13:09 +0300

    Основные доменные модели

---
+++ src/GitHubProjectSearcher.Domain/Query.cs
@@ -0,0 +1,13 @@
+﻿namespace GitHubProjectSearcher.Domain
+{
+    /// <summary>
+    /// Прилетевший запрос на поиск
+    /// </summary>
+    public class Query
+    {
+        public int QueryId { get; set; }
+        public string SearchString { get; set; }
+        public ICollection<Card> Cards { get; set; }
+
+    }
+}
