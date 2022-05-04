8c61d819fbbf9d3b988193fb5184e80a50bb6011
Ильшат Галиев <galiew.work@mail.ru>
2022-05-04 21:15:04 +0300

    Application слой приложения

---
+++ src/GitHubProjectSearcher.Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using FluentValidation;
+using GitHubProjectSearcher.Application.Common.Behaviours;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GitHubProjectSearcher.Application
+{
+    public static  class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssemblies(new[] { Assembly.GetExecutingAssembly() });
+            services.AddTransient(typeof(IPipelineBehavior<,>),
+                typeof(ValidationBehavior<,>));
+            return services;
+        }
+    }
+}
